9c4d0b62b72f63e11baa1f2c61892478ab557538
lyainevan <102477731+lyainevan@users.noreply.github.com>
2020-09-13 12:02:44 +0000

    Resolved merge conflict by incorporating both suggestions.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { observer } from "mobx-react-lite";
-import Home f
+import Home from "./components/screen
