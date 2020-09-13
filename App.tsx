c2545de4d4e0da65953f08c8992cdca9596a13f9
lyainevan <102477731+lyainevan@users.noreply.github.com>
2020-09-13 18:23:42 +0000

    added install target

--- App.tsx
+++ App.tsx
@@ -2,4 +2,5 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { observer } from "mobx-react-lite";
 import Home from "./components/screens/Home";
-import Theme from 
+import Theme from "./modules/theme";
+imp
