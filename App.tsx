7b7884ab53f55b63893c02c24f0b3a5a618bbb8f
lyainevan <102477731+lyainevan@users.noreply.github.com>
2020-09-13 16:30:51 +0000

    cleaned up dependencies

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { observer } from "mobx-react-lite";
-import Home from "./components/screen
+import Home from "./components/screens/Home";
+import Theme from 
