771fbc35fcc5a6d8b947234f770a0c9fc5ccbcdd
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-03-18 12:09:14 +0000

    update vendor

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-
+    plugins: ['react-native-reanimated/plugin']
+  };
+};
