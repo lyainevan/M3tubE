3015afb2402cbb446e82aa8bb2e7ff7b3cb00a6b
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-03-22 20:59:48 +0000

    Fix indentation issues

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,4 +1,5 @@
 import { observer } from "mobx-react-lite";
 import { Pressable, StyleSheet, View, Share } from "react-native";
 import CFText, { CFTypography } from "./CFText";
-import * as WebBrowser from "expo-web-browse
+import * as WebBrowser from "expo-web-browser";
+import Theme from "../modules/t
