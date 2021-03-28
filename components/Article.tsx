fafe9a41dcb6462d9348fb732fe4f020d7940cae
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-03-28 09:41:12 +0000

    template added

--- components/Article.tsx
+++ components/Article.tsx
@@ -2,4 +2,5 @@ import { observer } from "mobx-react-lite";
 import { Pressable, StyleSheet, View, Share } from "react-native";
 import CFText, { CFTypography } from "./CFText";
 import * as WebBrowser from "expo-web-browser";
-import Theme from "../modules/t
+import Theme from "../modules/theme";
+import { useContext } fr
