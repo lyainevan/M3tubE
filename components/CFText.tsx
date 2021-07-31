a90d533e9d1564d66fb1129a7de001437277b1b7
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-07-31 09:46:48 +0000

    Update features

--- components/CFText.tsx
+++ components/CFText.tsx
@@ -1,4 +1,6 @@
 import { prop } from "ramda";
 import { ReactNode } from "react";
 import { StyleProp, StyleSheet, Text, TextProps, TextStyle } from "react-native";
-import Theme from "../modules/the
+import Theme from "../modules/theme";
+
+export enum CFTypography 
