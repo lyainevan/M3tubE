3c417453b5f24ef9406fe97d7735cc2667581201
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-07-31 14:03:50 +0000

    Heartbeat test

--- components/CFText.tsx
+++ components/CFText.tsx
@@ -3,4 +3,6 @@ import { ReactNode } from "react";
 import { StyleProp, StyleSheet, Text, TextProps, TextStyle } from "react-native";
 import Theme from "../modules/theme";
 
-export enum CFTypography 
+export enum CFTypography {
+    BASE = "base",
+    H1 = "h
