06b9f91919f595abf03ed6d3f77a525c1e036932
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-09-14 22:55:43 +0000

    Update documents.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
 import { DrawerScreenProps } from "@react-navigation/drawer";
 import React from "react";
-import { Pressable, View, StyleSheet } from "react
+import { Pressable, View, StyleSheet } from "react-native";
+import { RootDrawerPar
