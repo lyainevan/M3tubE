831a44511f7ef4a223a9856eccac2cee01376ec0
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-09-22 19:22:31 +0000

    Version 0

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,4 +21,4 @@ const Header = ({
                 <Pressable
                     onPress={navigation.openDrawer}
                     hitSlop={20}
-                    style={{ 
+                    style={{ paddingEnd: Theme.spacing.sm
