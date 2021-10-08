7cfa760978f4ff0e42ac7f5578b3b8e7187f259e
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-10-08 21:50:30 +0000

    iOS

--- components/Loaders/ArticleLoader.tsx
+++ components/Loaders/ArticleLoader.tsx
@@ -5,3 +5,20 @@ import { ViewProps } from "react-native"
 
 const ArticleLoader = (props: ViewProps) => (
   <ContentLoader 
+    speed={2}
+    width={350}
+    height={700}
+    viewBox="6 0 350 700"
+    backgroundColor="#292929"
+    foregroundColor="#646464"
+    {...props}
+  >
+    <Rect x="0" y="20" rx="0" ry="0" width="400" height="100" /> 
+    <Rect x="0" y="140" rx="0" ry="0" width="400" height="100" />
+    <Rect x="0" y="260" rx="0" ry="0" width="400" height="100" />
+    <Rect x="0" y="380" rx="0" ry="0" width="400" height="100" />
+    <Rect x="0" y="500" rx="0" ry="0" width="400" height="100" />
+  </ContentLoader>
+)
+
+export default ArticleLoader
