09a5d04bed728dbfbb13c0a33a0985c193b10b82
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-10-09 08:31:45 +0000

    Rebuild docs

--- components/Loaders/HeaderLoader.tsx
+++ components/Loaders/HeaderLoader.tsx
@@ -1,2 +1,20 @@
 
 import React from "react"
+import ContentLoader, { Rect } from "react-content-loader/native"
+import { ViewProps } from "react-native"
+
+const HeaderLoader = (props: ViewProps) => (
+  <ContentLoader 
+    speed={2}
+    width={350}
+    height={200}
+    viewBox="0 0 350 200"
+    backgroundColor="#292929"
+    foregroundColor="#646464"
+    {...props}
+  >
+    <Rect x="0" y="100" rx="0" ry="0" width="400" height="100" /> 
+  </ContentLoader>
+)
+
+export default HeaderLoader
