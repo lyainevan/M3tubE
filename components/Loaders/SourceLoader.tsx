e4a0c8da3d3614a18ffc244cbd91601b6e7fe055
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-10-14 06:11:39 +0000

    More style tweaks

--- components/Loaders/SourceLoader.tsx
+++ components/Loaders/SourceLoader.tsx
@@ -2,4 +2,5 @@ import React from "react";
 import ContentLoader, { Circle } from "react-content-loader/native";
 import { ViewProps } from "react-native";
 
-const SourceLoader = (props: ViewProps
+const SourceLoader = (props: ViewProps) => (
+    <ContentLoader
