25280f63e86fc873f7c6e8329ffd25df02832b0c
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-10-26 06:11:19 +0000

    Release 1.6

--- components/SourceArticles.tsx
+++ components/SourceArticles.tsx
@@ -25,3 +25,43 @@ const SourceArticles = observer(() => {
         try {
             if (!activeSource)
                 throw new Error("No active source to fetch articles for.");
+            const { feedUrl, _id: sourceId } = activeSource;
+            const response = await fetch(ARTICLES_URL, {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    feedUrl,
+                }),
+            });
+            const articles: ReadonlyArray<CFArticle> = await response.json();
+
+            coinFeedStore.updateArticleStore(sourceId, articles);
+            return articles;
+        } catch (error) {
+            console.error(
+                `Error occcured while fetching articles for ${sourceId}: ${error}`
+            );
+        }
+    };
+
+    const handleRefresh = async () => {
+        setRefreshing(true);
+        if (sourceId) await fetchArticles();
+        setRefreshing(false);
+        await fetchSources(coinFeedStore);
+    };
+
+    useEffect(() => {
+        if (sourceId && !coinFeedStore.articleCountForActiveSource) {
+            fetchArticles();
+        }
+    });
+
+    return (
+        <FlatList
+            data={activeArticles}
+            renderItem={(prop) => <Article {...prop}></Article>}
+            keyExtractor={({ link }) => link}
