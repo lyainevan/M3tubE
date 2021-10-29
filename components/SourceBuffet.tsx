a725b0df23a4ed9e17d5c12e4c74757b88681395
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-10-29 19:12:41 +0000

    Header update

--- components/SourceBuffet.tsx
+++ components/SourceBuffet.tsx
@@ -1,4 +1,4 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { observer } from "mobx-react-lite";
 import React, { useContext, useEffect, useRef } from "react";
-import { StyleSheet, 
+import { StyleSheet, FlatList } from "react-nati
