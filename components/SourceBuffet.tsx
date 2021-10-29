9f175e463db05283cd031c4ccc2184bfee97f825
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-10-29 05:51:32 +0000

    cmake stuff

--- components/SourceBuffet.tsx
+++ components/SourceBuffet.tsx
@@ -1,4 +1,4 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { observer } from "mobx-react-lite";
 import React, { useContext, useEffect, useRef } from "react";
-i
+import { StyleSheet, 
