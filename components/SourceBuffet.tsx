cd3b62f5291dc975d6c7eb4d4f2eac8949fa021f
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-10-30 20:18:30 +0000

    ignore compiled files

--- components/SourceBuffet.tsx
+++ components/SourceBuffet.tsx
@@ -2,4 +2,5 @@ import { LinearGradient } from "expo-linear-gradient";
 import { observer } from "mobx-react-lite";
 import React, { useContext, useEffect, useRef } from "react";
 import { StyleSheet, FlatList } from "react-native";
-import { Animated } fro
+import { Animated } from "react-native";
+import {
