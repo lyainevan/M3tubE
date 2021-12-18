b0555d939c3e572f76026c37a84d977ea39c4fd1
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-12-18 17:29:53 +0000

    fixed commit

--- components/SourceConfigure.tsx
+++ components/SourceConfigure.tsx
@@ -2,4 +2,5 @@ import { Pressable, View, StyleSheet } from "react-native";
 import { RenderItemParams } from "react-native-draggable-flatlist";
 import Theme from "../modules/theme";
 import CFText from "./CFText";
-import { Source } from "./So
+import { Source } from "./SourceBuffet";
+import SourceI
