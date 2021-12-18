9f89086fc75ba967879c15b38c57d5455e5dc837
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-12-18 06:42:48 +0000

    some more progress

--- components/SourceConfigure.tsx
+++ components/SourceConfigure.tsx
@@ -1,3 +1,4 @@
 import { Pressable, View, StyleSheet } from "react-native";
 import { RenderItemParams } from "react-native-draggable-flatlist";
-import Theme from "../modul
+import Theme from "../modules/theme";
+import CFText 
