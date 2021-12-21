cc479cf18012ecab773c2fa0f5e3d85b24448475
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-12-21 06:37:33 +0000

    Prepare 2.2.0 release

--- components/SourceConfigure.tsx
+++ components/SourceConfigure.tsx
@@ -3,4 +3,5 @@ import { RenderItemParams } from "react-native-draggable-flatlist";
 import Theme from "../modules/theme";
 import CFText from "./CFText";
 import { Source } from "./SourceBuffet";
-import SourceImage from "./SourceImage
+import SourceImage from "./SourceImage";
+import { MaterialCommunityIco
