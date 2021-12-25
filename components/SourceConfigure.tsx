9ebf499d02b74aa4c3e12685592e2b247176a240
lyainevan <102477731+lyainevan@users.noreply.github.com>
2021-12-25 14:06:45 +0000

    v1.0.13

--- components/SourceConfigure.tsx
+++ components/SourceConfigure.tsx
@@ -11,4 +11,4 @@ import { observer } from "mobx-react-lite";
 import { CoinFeedStoreContext } from "../modules/CoinFeedStore";
 
 const SourceConfigure = observer(
-    ({ it
+    ({ item, isActive, drag }: RenderItemParams<
