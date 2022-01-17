5fde4c913ea2adcf5114b03e9e6239a0d6412bc1
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-01-17 06:48:29 +0000

    New translations template.pot (Chinese Simplified)

--- components/SourceConfigure.tsx
+++ components/SourceConfigure.tsx
@@ -42,4 +42,5 @@ const SourceConfigure = observer(
                                 ? {}
                                 : { color: Theme.color.gray }
                         }
-                    
+                    >
+                       
