81cef11dc8af6aeed350f99375499a3c6d9727a8
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-02-27 19:31:48 +0000

    I18n: Update translation sr (100%).

--- components/SourceHeader.tsx
+++ components/SourceHeader.tsx
@@ -3,4 +3,5 @@ import { Linking, Pressable, StyleSheet, View } from "react-native";
 import React, { useContext } from "react";
 import { CoinFeedStoreContext } from "../modules/CoinFeedStore";
 import CFText, { CFTypography } from "./CFText";
-import Theme 
+import Theme from "../modules/theme";
+import Divi
