e61187fb65beadfc3eaffbf5efd3f08e11efe0bf
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-02-27 11:30:45 +0000

    add install.sh

--- components/SourceHeader.tsx
+++ components/SourceHeader.tsx
@@ -1,4 +1,5 @@
 import { observer } from "mobx-react-lite";
 import { Linking, Pressable, StyleSheet, View } from "react-native";
 import React, { useContext } from "react";
-import { CoinFeedStoreContext } from "../modules
+import { CoinFeedStoreContext } from "../modules/CoinFeedStore";
+import CFTe
