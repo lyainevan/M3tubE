026b0fb87b149d4b3f52bcd7e9c1b81291d52f4b
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-02-27 12:06:19 +0000

    conditions

--- components/SourceHeader.tsx
+++ components/SourceHeader.tsx
@@ -2,4 +2,4 @@ import { observer } from "mobx-react-lite";
 import { Linking, Pressable, StyleSheet, View } from "react-native";
 import React, { useContext } from "react";
 import { CoinFeedStoreContext } from "../modules/CoinFeedStore";
-import CFTe
+import CFText, { CFTypography } from ".
