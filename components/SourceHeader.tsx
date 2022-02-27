662e30c596ac8de933be1b1cbe0ef5d229b8b027
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-02-27 19:28:37 +0000

    Fix Rubocop offenses

--- components/SourceHeader.tsx
+++ components/SourceHeader.tsx
@@ -2,4 +2,5 @@ import { observer } from "mobx-react-lite";
 import { Linking, Pressable, StyleSheet, View } from "react-native";
 import React, { useContext } from "react";
 import { CoinFeedStoreContext } from "../modules/CoinFeedStore";
-import CFText, { CFTypography } from ".
+import CFText, { CFTypography } from "./CFText";
+import Theme 
