34134708953c0a14ed86ce320f2cd59b5d001811
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-04-01 19:24:53 +0000

    Content-Editor changes

--- components/SourceHeader.tsx
+++ components/SourceHeader.tsx
@@ -48,4 +48,5 @@ const SourceHeader = observer<DrawerScreenProps<RootDrawerParamList>>(
                             color={Theme.color.gray}
                         />
                     </Pressable>
-                
+                    <Divider
+             
