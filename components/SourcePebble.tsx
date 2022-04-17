1cb21a8b5c9ea762a7febc945abc12fe8ccb97ad
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-04-17 16:28:10 +0000

    Use constants

--- components/SourcePebble.tsx
+++ components/SourcePebble.tsx
@@ -3,3 +3,4 @@ import { observer } from "mobx-react-lite";
 import React, { useContext } from "react";
 import { Pressable, StyleSheet, View } from "react-native";
 import { CoinFeedStoreContext } from "../modules/CoinFeedStore";
+import { toSourceImage
