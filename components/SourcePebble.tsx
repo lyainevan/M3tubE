b1889d124d7f891dc46b856186f22b4f74c869ef
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-04-28 10:03:29 +0000

    bench

--- components/SourcePebble.tsx
+++ components/SourcePebble.tsx
@@ -20,4 +20,6 @@ const Highlight = ({ isActive, color }: { isActive: boolean; color: string }) =>
 const SourcePebble = observer(
     ({ item, index }: { item: Source; index: number }) => {
         const coinFeedStore = useContext(CoinFeedStoreContext);
-        const isActive = coinFeedStore.activeSourc
+        const isActive = coinFeedStore.activeSource?._id === item._id;
+
+  
