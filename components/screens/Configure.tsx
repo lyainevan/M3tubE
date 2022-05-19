d19db53f7119db57a344786269ca492882e7210c
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-05-19 07:52:49 +0000

    Idea

--- components/screens/Configure.tsx
+++ components/screens/Configure.tsx
@@ -1,4 +1,4 @@
 import { StyleSheet, View } from "react-native";
 import Theme from "../../modules/theme";
 import DraggableFlatList from "react-native-draggable-flatlist";
-import { obse
+import { observer } from "mobx-react-l
