7b6f7ee9cec3fcdc0ec8bb8dfc958f14a95e39f5
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-05-20 12:07:47 +0000

    docs: update readme

--- components/screens/Configure.tsx
+++ components/screens/Configure.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View } from "react-native";
 import Theme from "../../modules/theme";
 import DraggableFlatList from "react-native-draggable-flatlist";
-import { observer } from "mobx-react-l
+import { observer } from "mobx-react-lite";
+import React, { us
