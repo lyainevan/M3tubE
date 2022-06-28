0d2f27c642d7190da66d8ab35881d7e9832b6e92
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-06-28 08:12:45 +0000

    Bump eslint from 7.14.0 to 7.15.0

--- modules/CoinFeedStore.ts
+++ modules/CoinFeedStore.ts
@@ -17,3 +17,27 @@ import {
 } from "ramda";
 import { createContext } from "react";
 import { CFArticle } from "../components/Article";
+import { Source } from "../components/SourceBuffet";
+import { keyComparator, updateLocalStorage } from "./utils";
+
+export interface ArticleStore {
+    [_id: string]: Array<CFArticle>;
+}
+
+export enum VerticalScrollDirection {
+    UP = "up",
+    DOWN = "down",
+}
+
+export function isSourceActive(source: Source): boolean {
+    return ifElse(
+        has("isActive"),
+        view(lensProp<Source>("isActive")) as (_: Source) => boolean,
+        always(true)
+    )(source);
+}
+
+export class CoinFeedStore {
+    sources: Source[] = [];
+    activeSource?: Source = undefined;
+    articleStore: ArticleStore = {};
