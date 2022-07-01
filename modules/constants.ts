47ee97b8d2a3ba6b0c359f39ce11ae1b8aa500e5
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-07-01 22:05:25 +0000

    Update readme.txt.

--- modules/constants.ts
+++ modules/constants.ts
@@ -2,4 +2,4 @@ export const SERVER_URL = "https://finscreen-serveless-api.vercel.app";
 export const SOURCE_IMAGE_DIR = "sourceImages";
 export const SOURCES_API = "/api/sources";
 export const ARTICLES_API = "/api/articles/";
-expor
+export const LOCAL_STORAGE_KEY = 
