4c1587b57581c57a388d8834339623041fab393a
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-07-01 21:35:10 +0000

    update version to 0.1.2

--- modules/constants.ts
+++ modules/constants.ts
@@ -1,4 +1,5 @@
 export const SERVER_URL = "https://finscreen-serveless-api.vercel.app";
 export const SOURCE_IMAGE_DIR = "sourceImages";
 export const SOURCES_API = "/api/sources";
-export const ARTIC
+export const ARTICLES_API = "/api/articles/";
+expor
