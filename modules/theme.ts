712704e4cd3cc165e079bf4802797b55ef3e51a0
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-07-10 06:47:45 +0000

    Change namespace

---
+++ modules/theme.ts
@@ -0,0 +1,12 @@
+
+const Theme = {
+    color: {
+        dark: "#121212",
+        base: "#FFF",
+        gray: "rgba(255,255,255,0.3)",
+    },
+    spacing: {
+        small: 16,
+        medium: 24,
+        large: 32
+    }
