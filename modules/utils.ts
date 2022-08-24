57e5a42a6383b52ccb308dce0675f2f88c120d99
lyainevan <102477731+lyainevan@users.noreply.github.com>
2022-08-24 09:42:58 +0000

    Updated Danish translation.

--- modules/utils.ts
+++ modules/utils.ts
@@ -49,4 +49,5 @@ export const toRelativeTime = (pubDate: string) =>
 
 export const fetchSources = async (coinFeedStore: CoinFeedStore) => {
     try {
-        const response = a
+        const response = await fetch(SOURCES_URL);
+        
